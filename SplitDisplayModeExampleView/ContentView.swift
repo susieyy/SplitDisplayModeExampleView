857a999ba40ad4e7442aa8f7946b0a016611bd5c
susieyy <y.sugigami@gmail.com>
2020-07-31 16:37:07 +0900

    Initial Commit

---
+++ SplitDisplayModeExampleView/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SplitDisplayModeExampleView
+//
+//  Created by yohei sugigami on 2020/07/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SplitDisplayModeExampleView/SplitDisplayModeExampleViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitDisplayModeExampleViewApp.swift
+//  SplitDisplayModeExampleView
+//
+//  Created by yohei sugigami on 2020/07/31.
+//
+
+import SwiftUI
+
+@main
+struct SplitDisplayModeExampleViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
